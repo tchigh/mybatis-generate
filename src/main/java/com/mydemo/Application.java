a0e0032fdba00d6344320f1fb515a27d4bbc61af
zpy <772935769@qq.com>
2021-01-27 17:52:33 +0800

    gitignore

---
+++ src/main/java/com/mydemo/Application.java
@@ -0,0 +1,18 @@
+package com.mydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: Application
+ * @Author: zpy
+ * @Date: 2020/11/6 11:25
+ * @Description:
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/com/mydemo/PlusGen/MpG.java
@@ -0,0 +1,68 @@
+package com.mydemo.PlusGen;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MpG {
+    public static void main(String[] args) {
+        //1. 全局配置
+        GlobalConfig config = new GlobalConfig();
+        config.setActiveRecord(false) // 是否支持AR模式
+                .setAuthor("zpy") // 作者
+                .setOutputDir("E:\\data\\me\\my-generator\\src\\main\\java\\com\\mydemo\\base") // 生成路径
+                .setFileOverride(true) // 文件覆盖
+                .setIdType(IdType.AUTO) // 主键策略
+                .setServiceName("%sService") // 设置生成的service接口的名字的首字母是否为I
+                // IUserService
+                .setBaseResultMap(true)
+                .setBaseColumnList(true)
+                .setOpen(false);
+
+        //2. 数据源配置
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        dsConfig.setDbType(DbType.MYSQL) // 设置数据库类型
+                .setDriverName("com.mysql.jdbc.Driver")
+                .setUrl("jdbc:mysql://172.17.38.227/szbaoan?characterEncoding=UTF-8")
+                .setUsername("root")
+                .setPassword("Intel@123456");
+
+        //3. 策略配置
+        StrategyConfig stConfig = new StrategyConfig();
+        stConfig.setCapitalMode(true) //全局大写命名
+                .setDbColumnUnderline(true) // 指定表名 字段名是否使用下划线
+                .setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
+                .setTablePrefix("tb_")
+                .setInclude("ait_accident"); // 生成的表
+
+        //4. 包名策略配置
+        PackageConfig pkConfig = new PackageConfig();
+        pkConfig.setParent("com.cun.plus")
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+                .setEntity("entity")
+                .setXml("mapper");
+
+        //5. 整合配置
+        AutoGenerator ag = new AutoGenerator();
+        ag.setGlobalConfig(config)
+                .setDataSource(dsConfig)
+                .setStrategy(stConfig)
+                .setPackageInfo(pkConfig);
+
+        //6. 执行
+        ag.execute();
+    }
+
+}
